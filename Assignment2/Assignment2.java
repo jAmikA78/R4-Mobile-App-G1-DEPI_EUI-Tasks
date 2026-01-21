3f4c1e58b83a4e9bf46b90eb1df6fb6487f67866
Aliyah Belal <alyaaabelal@gmail.com>
2026-01-21 15:15:49 +0200

    Add files via upload

---
+++ Assignment2/Assignment2.java
@@ -0,0 +1,31 @@
+
+
+package com.mycompany.assignment2;
+
+
+public class Assignment2 {
+
+    public static void main(String[] args) {
+        
+        //student assignment
+        Student s1 = new Student("aliyah",19,"cs");
+        s1.printInfo();
+        
+         System.out.println("__________________________________________________");
+        
+        // complex assignments
+  
+           Complex c1 = new Complex(7,6);
+        c1.add(2,3);
+        c1.printsum();
+        c1.subtract(2,3);
+        c1.printsub();
+
+
+    }
+}
+
+        
+        
+    
+

7541f62014dffda17f093ef09771dd68e44d3f98
Heba2007-yn <hebaemam2007@gmail.com>
2026-01-23 15:08:34 +0200

    Assigment1

---
+++ src/main/java/com/mycompany/evenodd/EvenOdd.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evenodd;
+
+/**
+ *
+ * @author Heba
+ */ 
+import java.util.Scanner;
+
+public class EvenOdd {
+     
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+System.out.println("Enter number:");
+int number=input.nextInt();
+if (number % 2 == 0){ 
+    System.out.println("Its even");
+}
+else{ System.out.println("Its odd");
+
+    
+    } System.out.println("Hello java");
+    }
+}
